e3731ed9f33f7eaf93ed3529c912ec7d53b83777
shea6utte <an6sty@gmail.com>
2022-02-16 20:08:39 +0800

    Edit MAIN method

--- src/AirplaneSeatAssignment.java
+++ src/AirplaneSeatAssignment.java
@@ -1,6 +1,19 @@
-public class AirplaneSeatAssignment {
+import java.util.Scanner;
+
+public class AirplaneSeatAssignment
+{
     public static void main(String[] args) throws Exception 
     {
+        System.out.println("Welcome to Ymirates Airline!");
+        System.out.println("");
+        System.out.println("Enter the following information:");
     
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        System.out.println("Select ticket type " + ("(First Class, Business Class, or Economy Class") + input);
+
+        
+        scanner.close();
     }
 }
