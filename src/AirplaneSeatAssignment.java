5c22b3045e52de0d78c7b8e0705ffea1f2326674
shea6utte <an6sty@gmail.com>
2022-02-16 23:51:02 +0800

    Edited Methods

--- src/AirplaneSeatAssignment.java
+++ src/AirplaneSeatAssignment.java
@@ -3,39 +3,67 @@ import java.util.Scanner;
 
 public class AirplaneSeatAssignment
 {
-    public static void Tickets (String TicketType)
+    /**
+     * initTicket Method
+     * @param TicketType
+     *  Display and selection of ticket types.
+     */
+    static String[] initTickets()
     {
-        String[] ticket = {"First Class","Business Class",
-                           "Economy Class"};
+        String[] ticket = {"First Class (Row 1-2)","Business Class (Row 3-7)",
+        "Economy Class (Row 8-13)"};
 
-        System.out.println(ticket[0] + "Row 1 to 2");
-        System.out.println(ticket[1] + "Row 3 to 7");
-        System.out.println(ticket[2] + "Row 8 to 13");
-
-        Scanner scanner = new Scanner(System.in);
-
-        String ticketChosen = scanner.next();
-        System.out.println("Select ticket type: " + ticketChosen);
-
-        Scanner.close();
+        for (int i = 0; i < 3; i++)
+            System.out.println(ticket[i]);
+        
+        return ticket;
     }
+    
+    static void displayTickets ()
+    {
+        Scanner scanner = new Scanner(System.in);
+        String TicketType = scanner.nextLine();
 
-    public static void Seat (String DesiredSeat)
+        System.out.println("Select ticket type: " + TicketType);
+    }
+    /**
+     * 
+     * @param args
+     * @throws Exception
+     */
+    
+    static void initSeat (char[][] airlineSeat)
     {
         Random random = new Random();
-        int[][] airlineSeat = new int [13][9];
 
-        for(int x = 0; x < 13; x++)
-            for (int y = 0; y < 9; y++)
-                airlineSeat[x][y] = random.nextInt();
-
-        
+        for (int x = 0; x < 13; x++)
+            for (int y = 0; y < 6; y++)
+                airlineSeat[x][y] = (char) random.nextInt();
+   
     }
 
+    static void SeatDisplay (int[][] initSeat)
+    {
+        for (int a = 0; a < 13; a++)
+            for (int b = 0; b < 6; b++)
+                System.out.println(initSeat[a][b] + "\t");
+    }
     public static void main(String[] args) throws Exception 
     {
+        System.out.println("--------------------------------");
         System.out.println("Welcome to Ymirates Airline!");
+        System.out.println("--------------------------------");
+        System.out.println("Enter the following information.");
+        System.out.println("--------------------------------");
+
+        System.out.println("Ticket Types: ");
         System.out.println("");
-        System.out.println("Enter the following information:");
+        initTickets();
+
+
+        char[][] airlineSeat = new char [13][9];
+        initSeat(airlineSeat);
+
+        scanner.close();
     }
 }
