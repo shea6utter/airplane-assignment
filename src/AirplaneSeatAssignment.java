8a83dd616f881341d726e0e5a10a61b0da5f82f9
shea6utte <an6sty@gmail.com>
2022-02-16 20:58:48 +0800

    Added Ticket method and Modified Seat method

--- src/AirplaneSeatAssignment.java
+++ src/AirplaneSeatAssignment.java
@@ -1,24 +1,41 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class AirplaneSeatAssignment
 {
-    public static void DesiredSeat (String seatChosen)
+    public static void Tickets (String TicketType)
+    {
+        String[] ticket = {"First Class","Business Class",
+                           "Economy Class"};
+
+        System.out.println(ticket[0] + "Row 1 to 2");
+        System.out.println(ticket[1] + "Row 3 to 7");
+        System.out.println(ticket[2] + "Row 8 to 13");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String ticketChosen = scanner.next();
+        System.out.println("Select ticket type: " + ticketChosen);
+
+        Scanner.close();
+    }
+
+    public static void Seat (String DesiredSeat)
     {
         Random random = new Random();
         int[][] airlineSeat = new int [13][9];
+
+        for(int x = 0; x < 13; x++)
+            for (int y = 0; y < 9; y++)
+                airlineSeat[x][y] = random.nextInt();
+
+        
     }
+
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Welcome to Ymirates Airline!");
         System.out.println("");
         System.out.println("Enter the following information:");
-    
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-
-        System.out.println("Select ticket type " + ("(First Class, Business Class, or Economy Class") + input);
-
-        
-        scanner.close();
     }
 }
